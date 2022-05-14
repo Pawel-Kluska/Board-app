4cfcaa8c80cce659ab83a4c6a64a2be3422f644c
Pawel Kluska <pawel.kluska256@gmail.com>
2022-05-14 10:46:46 +0200

    add registration and login layout

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(2, 30)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    address = StringField('Address', validators=[Length(0, 12)])
+
+    phone = StringField('Phone', validators=[Length(9, 12)])
+
+    submit = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    remember = BooleanField("Remember me")
+
+    submit = SubmitField('Login')
