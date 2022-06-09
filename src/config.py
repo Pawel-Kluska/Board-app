f6763ee3648c378b231c9b91f1ebcdcf23467945
Pawel Kluska <pawel.kluska256@gmail.com>
2022-06-09 12:39:27 +0200

    Database changed

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@
 class Config:
     SECRET_KEY = '58b35f5c02f37f5bd505bcb95d0433fe'
     # SQLALCHEMY_DATABASE_URI = 'mysql://flask_user:flask_password@localhost/blog'
-    SQLALCHEMY_DATABASE_URI = 'mysql://b0c030fa740e0b:942a5ed3@eu-cdbr-west-02.cleardb.net/heroku_5567a239a35952c?reconnect=true'
+    SQLALCHEMY_DATABASE_URI = 'mysql://b0c030fa740e0b:942a5ed3@eu-cdbr-west-02.cleardb.net/heroku_5567a239a35952c'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
